5c5698f9e04ec825829fc42f92740b82f88b1ab7
rordev87 <rordev87@proton.me>
2019-06-10 15:52:17 +0300

    add validation for comment form

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,12 +10,26 @@ $(function () {
 
     $("#add-comment-form").submit(function (event) {
       event.preventDefault();
-      let values = $(this).serialize();
-      addNewComment(values);
-      $("#body-entry").val("");
-      $("#rating-entry").val("");
-      if ($("#no-comments-listed").length) {
-        $("#no-comments-listed").remove();
+      let commentBody = $("#body-entry");
+      let commentRating = $("#rating-entry");
+
+      if (commentBody.val() == "") {
+        commentBody.addClass("error")
+        $("#commentErrorText").css("display", "block");
+      } else if (commentRating.val() == null) {
+        commentRating.addClass("error")
+        $("#commentErrorText").css("display", "block");
+      } else {
+        $("#commentErrorText").css("display", "none");
+        commentBody.removeClass("error")
+        commentRating.removeClass("error")
+        let values = $(this).serialize();
+        addNewComment(values);
+        $("#body-entry").val("");
+        $("#rating-entry").val("");
+        if ($("#no-comments-listed").length) {
+          $("#no-comments-listed").remove();
+        }
       }
     })
   }
@@ -249,6 +263,7 @@ Restaurant.prototype.singleHTML = function () {
 function addNewComment(values) {
   let fullname = $("#user-fullname").attr("data-value");
   let avatar_url = $("#user-avatar-url").attr("data-value");
+
   var newComment = $.post('/comments', values);
   newComment.done(function (data) {
     comment =
