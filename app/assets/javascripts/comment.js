9ac48f62ffd2e83f7ebc90c91c852806fc7cc9d0
rordev87 <rordev87@proton.me>
2019-06-12 12:42:24 +0300

    add comments

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,10 +1,11 @@
-// comment
+// comment class
 class Comment {
   constructor(body, rating) {
     this.body = body;
     this.rating = rating;
   }
 
+  // validation method - checking for empty/null values
   valid() {
     if (this.body == "" || this.rating == null) {
       return false;
